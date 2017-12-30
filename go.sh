38c90666805f3a8d5b363e25108b7ab57c44f50e
李建赢 <lijianying12@gmail.com>
2017-12-30 08:58:40 +0800

    update go

--- go.sh
+++ go.sh
@@ -2,3 +2,6 @@ wget https://redirector.gvt1.com/edgedl/go/go1.8.5.linux-amd64.tar.gz
 tar xf go1.8.5.linux-amd64.tar.gz
 mv go /usr/local/
 rm -rf go1.8.5.linux-amd64.tar.gz
+echo 'export GOROOT=/usr/local/go' >> /root/.bashrc
+echo 'export GOPATH=/root/go' >> /root/.bashrc
+echo 'export PATH=$PATH:/root/go/bin/:/usr/local/go/bin/' >> /root/.bashrc
